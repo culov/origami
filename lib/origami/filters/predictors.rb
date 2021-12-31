5d0246024d8a31dca78bdcd07791f512a113a8fd
Ivan <anon@Ivans-iMac.local>
2021-12-31 13:41:51 -0800

    pipeline

--- lib/origami/filters/predictors.rb
+++ lib/origami/filters/predictors.rb
@@ -47,7 +47,7 @@ module Origami
 
             def self.included(receiver)
                 raise TypeError, "Predictors only applies to Filters" unless receiver.include?(Filter)
-            end
+    end
 
             #
             # Create a new predictive Filter.
